73c2931e1399fdb1dcd8a36aa7417b2fa39c9ff3
kartik13-kkok <chavankartik048@gmail.com>
2026-07-02 13:06:45 +0530

    updating adding live URL0

--- config/settings.py
+++ config/settings.py
@@ -118,5 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ALLOWED_HOSTS = [
+    "django-api.onrender.com",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
