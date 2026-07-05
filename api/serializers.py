5af5d3274b7395d25470c663570848e23bcb9edf
kartik13-kkok <chavankartik048@gmail.com>
2026-07-05 20:02:54 +0530

    Responsive ui

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = '__all__'   
